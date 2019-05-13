4f3f6ef930493752b0b15c8b82169401b2bda217
Deepika handa <deepika_handa@hotmail.com>
2019-05-14 05:56:41 +1000

    Intial commit

---
+++ src/main/java/com/java/spring/cloud/config/CloudServerMain.java
@@ -0,0 +1,16 @@
+package com.java.spring.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class CloudServerMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudServerMain.class, args);
+
+	}
+
+}
